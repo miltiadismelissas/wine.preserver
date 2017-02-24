6ca393b50e2a7f24593f3c0e07c1d0ff59569ed4
Miltiadis Melissas <mm260865@gmail.com>
2017-02-24 18:07:20 +0200

    Add files via upload

--- myfirstIOT.py
+++ myfirstIOT.py
@@ -51,7 +51,7 @@ def main():
     print ('starting...')
 
     baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
-    print baseURL
+    print (baseURL)
     
     while True:
         try:
@@ -67,7 +67,7 @@ def main():
 
             sleep(int(myDelay))
         except:
-            print 'exiting.'
+            print ('exiting.')
             break
 
 # call main
