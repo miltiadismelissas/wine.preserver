b7aff0df8d885d9dc870eefef24ce7fe8018e32c
Miltiadis Melissas <mm260865@gmail.com>
2017-02-26 17:56:24 +0200

    Add files via upload

--- wine.preserver.py
+++ wine.preserver.py
@@ -29,9 +29,9 @@ while True:
 	LEDpin= 4
 	
 	
-	GPIO.output(LEDpin,GPIO.LOW)
+	GPIO.output(LEDpin,GPIO.HIGH)
 	time.sleep(1)
-	GPIO.output(led,GPIO.HIGH)
+	GPIO.output(led,GPIO.LOW)
 	
 	
 	GPIO.setmode(GPIO.BCM)
